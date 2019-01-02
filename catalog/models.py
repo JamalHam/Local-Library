ad926a8144eaf56c610cc42ae555dde6e7bf04ae
JamalHam <hamilton.jamal.anthony@gmail.com>
2019-01-02 17:48:10 -0500

    added sum books

--- catalog/models.py
+++ catalog/models.py
@@ -27,7 +27,7 @@ class Book(models.Model):
     # Author as a string rather than object because it hasn't been declared yet in the file
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
 
-    summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
+    summary = models.TextField(max_length=4000, help_text='Enter a brief description of the book')
     isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
 
     # ManyToManyField used because genre can contain many books. Books can cover many genres.

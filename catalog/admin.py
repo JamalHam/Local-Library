ed57659663f53d72754a2642a859e40912d25a66
JamalHam <hamilton.jamal.anthony@gmail.com>
2019-01-04 18:40:26 -0500

    more changes to the admin interface

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,24 +2,40 @@ from django.contrib import admin
 from catalog.models import Author, Genre, Book, BookInstance, Language
 
 # Register your models here.
-admin.site.register(Book)
+"""admin.site.register(Book)
 admin.site.register(Author)
+admin.site.register(BookInstance)"""
 admin.site.register(Genre)
 admin.site.register(Language)
-admin.site.register(BookInstance)
+
 
 #define the admin class
 class AuthorAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
 #Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)    
 
 # Register the Admin classes for Book using the decorator (this does exactly the same thing as the admin.site.register() syntax):
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
 
-@admin.rergister(BookInstance)
-class BookInstanceAdmin(admmin.ModelAdmin)
-    pass
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(BookInstance) 
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back', 'imprint')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )

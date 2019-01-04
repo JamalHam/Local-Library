fa0fd85b5ac7225fd41e5c83d8d276534a831e2e
JamalHam <hamilton.jamal.anthony@gmail.com>
2019-01-04 18:03:53 -0500

    model admin classes

--- catalog/admin.py
+++ catalog/admin.py
@@ -7,3 +7,19 @@ admin.site.register(Author)
 admin.site.register(Genre)
 admin.site.register(Language)
 admin.site.register(BookInstance)
+
+#define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+#Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)    
+
+# Register the Admin classes for Book using the decorator (this does exactly the same thing as the admin.site.register() syntax):
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+@admin.rergister(BookInstance)
+class BookInstanceAdmin(admmin.ModelAdmin)
+    pass

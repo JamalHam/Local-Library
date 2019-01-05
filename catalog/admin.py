cdbf60abf8f594df6a89a05d8eed9828c74dc91f
JamalHam <hamilton.jamal.anthony@gmail.com>
2019-01-05 16:58:28 -0500

    admin site changes

--- catalog/admin.py
+++ catalog/admin.py
@@ -8,11 +8,16 @@ admin.site.register(BookInstance)"""
 admin.site.register(Genre)
 admin.site.register(Language)
 
+class BooksInline(admin.StackedInline):
+    """Defines format of inline book insertion (used in AuthorAdmin)"""
+    model = Book
+    classes = ['collapse']
 
 #define the admin class
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
 
 #Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)    
@@ -29,6 +34,7 @@ class BookAdmin(admin.ModelAdmin):
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance) 
 class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('status', 'due_back', 'id')
     list_filter = ('status', 'due_back', 'imprint')
 
     fieldsets = (

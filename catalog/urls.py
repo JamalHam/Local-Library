1678751ac8e9c5e0e451ed82d2bd3ad05bb58b0e
JamalHam <hamilton.jamal.anthony@gmail.com>
2019-01-26 18:45:30 -0500

    login and delete authors

--- catalog/urls.py
+++ catalog/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     path('allbooks/', views.AllBorrowedBooks.as_view(), name="Librarian-unique"),
     path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
-
+    path('author/create/', views.AuthorCreate.as_view(), name='author_create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author_update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author_delete'),
 ]
